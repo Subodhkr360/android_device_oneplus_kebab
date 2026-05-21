2a6dd6efcc3069f1320dc15410c836c6f7416f63
rahulsingh0012 <rahulrajsn35@gmail.com>
2026-05-21 08:07:41 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -51,6 +51,15 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.lunaris.maintainer=Rahul Singh
 
+#Set udfp
+TARGET_CUSTOM_UDFPS := true
+
+#GSM Flag
+WITH_GMS := true
+
+#Stock dailer
+TARGET_SUPPORTS_GOOGLE_TELEPHONY := false
+
 # Touch
 $(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,INCLUDE_DIR,$(LOCAL_PATH)/touch/include)
 
