1ab4f58cec85bd62b93fd0ec76e64bce53b4790f
rahulsingh0012 <rahulrajsn35@gmail.com>
2026-05-21 00:39:01 -0700

    Update lineage_kebab.mk

--- lineage_kebab.mk
+++ lineage_kebab.mk
@@ -15,6 +15,19 @@ $(call inherit-product, device/oneplus/kebab/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# MistOs Flags
+MISTOS_MAINTAINER := RahulSingh
+WITH_GMS := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := false
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+MIST_BUILD_TYPE := OFFICIAL
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_ENABLE_BLUR := true
+TARGET_INCLUDE_ACCORD := false
+BYPASS_CHARGE_SUPPORTED := true
+
 PRODUCT_NAME := lineage_kebab
 PRODUCT_DEVICE := kebab
 PRODUCT_MANUFACTURER := OnePlus
